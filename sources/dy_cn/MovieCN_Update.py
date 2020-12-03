1685f964336ffa0d7fbe4d6f3377c690fb9a5bac
Herais <48284363+Herais@users.noreply.github.com>
2020-12-03 22:27:46 +0800

    0.9.0.0
    (1)Updated Registration Records as of 2020/12/01
    
    Expect major revision as the film department moves to a new platform.

--- sources/dy_cn/MovieCN_Update.py
+++ sources/dy_cn/MovieCN_Update.py
@@ -5,7 +5,7 @@ Created on Fri Apr 12 23:32:35 2019
 @author: VSurfacePro3
 """
 #%%
-import MovieCN
+import dy_Registration
 #from IO_Storage import File
 #%%
 # Check if Data Files exist, if not, create files
@@ -17,12 +17,19 @@ import MovieCN
 #GR = MovieCN.GetRegistration()
 
 # Update and Save Registration
-UR = MovieCN.UpdateRegistration()
+#links_of_publications = dyreg.links_of_publications()
+#links_of_registrations = dyreg.links_of_registrations()
+#contents_of_registrations= dyreg.contents_of_registrations()
+
+dyreg = dy_Registration.Registration()
+dyreg.update_records(save_update=True)
+
+#UR = MovieCN.update_records()
 #links_of_newregpub = UR.links_of_newpublications()
 #links_of_newreg = UR.links_of_newregistrations()
-contents_of_newreg = UR.contents_of_newregistrations(savefile=True)
+#contents_of_newreg = UR.contents_of_newregistrations(savefile=True)
 
 # Update and Save Release
-URS = MovieCN.UpdateRelease()
+#URS = MovieCN.UpdateRelease()
 #links_of_newrelpub = URS.links_of_newpublications()
-contents_of_newrelpub = URS.contents_of_newpublications(savefile=True)
+#contents_of_newrelpub = URS.contents_of_newpublications(savefile=True)
