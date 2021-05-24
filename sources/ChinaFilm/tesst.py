c6ad02798ac77277180feceb681b16ee584ae81d
Herais <48284363+Herais@users.noreply.github.com>
2021-05-23 18:30:41 -0700

    0.2.7

--- sources/ChinaFilm/tesst.py
+++ sources/ChinaFilm/tesst.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 from urllib.request import Request, urlopen
 import random
+import os
+import pandas as pd
 
 dy_reg = Registration()
 #%%
@@ -10,10 +12,16 @@ links_of_registrations = dy_reg.links_of_registrations(links_of_publications, sa
 #%%
 contents = dy_reg.contents_of_registrations(links_of_registrations[0:3])
 #%%
-batch_size = 50
-i = 0
+links_of_registrations = pd.read_csv('records/links_of_registrations.csv', encoding='utf-8-sig')
+batch_size = 25
+i = 300
+fp = 'D://Github//Content//sources//ChinaFilm//records//contents_of_registrations_test.csv'
 while i < len(links_of_registrations):
     records = dy_reg.contents_of_registrations(links_of_registrations[i:i+batch_size])
-    records.to_csv('/records/contents_of_registrations.csv', mode='a',encoding='utf-8-sig', index=False)
+    records.to_csv(fp, mode='a', header=(not os.path.exists(fp)), encoding='utf-8-sig', index=False)
     i += batch_size
-    print(i)
+    print(i, '-')
+dy_reg.driver.close()
+    #%%
+from sys import platform
+print(platform)
