24995543a97539b933d5369a611118a8476081d7
{Herais} <{veexu@ctcultures.com}>
2021-10-30 07:48:36 +0000

    from Colab Content

---
+++ sources/baike/baike_search.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 21:19:01 2021
+
+@author: VXhpUS
+"""
+from bs4 import BeautifulSoup
+from Baike import Baike
+import pandas as pd
+
+#%%
+baike = Baike()
+
+#%%
+str_item = '关于我妈的一切'
+res = baike.search(str_item)
+res1 = baike.search_for_content(str_item)
+
+#%%
+baike.get_talents(res)
+#%%
+pd.read_html(res)
