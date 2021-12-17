c7ee242742588df732737300e7a24e9f15932d62
Herais <48284363+Herais@users.noreply.github.com>
2021-12-17 14:45:10 -0800

    ifsp

--- sources/ifsp/peek/ifsp/ifsp/settings.py
+++ sources/ifsp/peek/ifsp/ifsp/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = 'ifsp'
 SPIDER_MODULES = ['ifsp.spiders']
 NEWSPIDER_MODULE = 'ifsp.spiders'
 
+SPLASH_URL = 'http://0.0.0.0:8050'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'ifsp (+http://www.yourdomain.com)'
@@ -44,18 +45,27 @@ COOKIES_ENABLED = False
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'ifsp.middlewares.IfspSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+    #'ifsp.middlewares.IfspSpiderMiddleware': 543,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     #'ifsp.middlewares.IfspDownloaderMiddleware': 543,
-    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+    #'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    #'scrapy_user_agents.middlewares
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
 }
 
+# Set custom DUPEFILTER_CLASS
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+# Set Scrapy HTTP Cache
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
